f25dd54b263ce999e03d9c6ec311adabbcef9a4d
erofcon <temirlan.shereuzhev@gmail.com>
2022-11-23 12:38:15 +0300

    v 0.06

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,16 @@
 FROM python:3.10-slim-buster
 
 RUN apt-get update
+RUN apt-get install -y git
 RUN apt install -y netcat
+RUN apt-get install -y git
 RUN python3 -m pip install --upgrade pip
 RUN python3 -m pip install psycopg2-binary
 
 
+RUN git clone https://github.com/erofcon/foton_handler.git /opt/foton_handler/
 
-COPY . /opt/foton_hundler
-
-WORKDIR /opt/foton_hundler
+WORKDIR /opt/foton_handler/
 RUN python3 -m pip install -r requirements.txt
 
 EXPOSE 8000
--- app/crud/controller_data.py
+++ app/crud/controller_data.py
@@ -8,7 +8,7 @@ from app.models.database import database
 async def get_controller_data_between_two_datetime(controller_id: int, start_datetime: datetime,
                                                    end_datetime: datetime):
     query = text(
-        f"""SELECT vin, vout, temp, charge, relay, status, create_data_datetime
+        f"""SELECT vin, vout, temp, charge, relay, create_data_datetime
                 FROM controller_data 
                 WHERE controller_id={controller_id} and create_data_datetime >= '{start_datetime.date()}'
                 AND create_data_datetime < '{(end_datetime + timedelta(days=1)).date()}' AND status=true order by 
