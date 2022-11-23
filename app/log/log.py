d8eca285edaa0cf8a014aad86c92f1359e5eac61
erofcon <temirlan.shereuzhev@gmail.com>
2022-11-23 15:12:42 +0300

    v 0.06.2

--- app/log/log.py
+++ app/log/log.py
@@ -5,7 +5,7 @@ def get_logger(name, level=logging.DEBUG) -> logging.Logger:
     return_format = "[%(levelname)s  %(name)s %(module)s:%(lineno)s - %(funcName)s() - %(asctime)s]\n\t %(message)s \n"
     datetime_format = "%d.%m.%Y %I:%M:%S %p"
 
-    filename = 'log/log.log'
+    filename = 'log.log'
 
     logging.basicConfig(format=return_format, datefmt=datetime_format, level=level,filename=filename)
 
